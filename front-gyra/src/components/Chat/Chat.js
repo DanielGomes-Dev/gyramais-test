5fea76206dee29b01d2e905e39db7a286c4cd53a
Daniel Gomes <danielgomes1@live.com>
2021-04-10 20:24:07 -0300

    feat: Componente Chat Criado

--- front-gyra/src/components/Chat/Chat.js
+++ front-gyra/src/components/Chat/Chat.js
@@ -1,8 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+import InfoBar from '../InfoBar/InfoBar';
+import Messages from '../Messages/Messages';
+
+import Input from '../Input/Input';
+
+import './Chat.css'
+
+let socket;
+
+const Chat = ({ location }) => {
+
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const ENDPOINT = 'http://localhost:3333/';
+
+
+  useEffect(() => {
+    const { name } = queryString.parse(location.search)
+
+    const connectionOptions = {
+      "force new connection": true,
+      "reconnectionAttempts": "Infinity",
+      "timeout": 10000,
+      "transports": ["websocket"]
+    };
+
+    socket = io.connect(ENDPOINT, connectionOptions);
+
+    setName(name);
+    socket.emit('join', name, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+
+  }, [ENDPOINT, location.search]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (message) socket.emit('sendMessage', message, () => setMessage(''));
+  }
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+
+
+
   return (
-    <h1>Chat</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar name={name} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+
+    </div>
+
   )
 }
 
