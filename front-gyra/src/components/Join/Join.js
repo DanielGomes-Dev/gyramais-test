4fac116ca13958614afdd33211a9e40c7e15f2e4
Daniel Gomes <danielgomes1@live.com>
2021-04-10 14:02:43 -0300

    wip: pagina Join Criada e Estilizada

--- front-gyra/src/components/Join/Join.js
+++ front-gyra/src/components/Join/Join.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Join.css';
 
 const Join = () => {
+
+  const [name, setName] = useState('');
+
   return (
-    <h1>Join</h1>
+    <div className="joinOuterContainer">
+
+      <div className="JoinInnerContainer">
+
+        <h1 className="heading">Chat</h1>
+
+        <div>
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+        </div>
+
+        <Link onClick={event => !name ? event.preventDefault() : null} to={`/chat?name=${name}`}>
+          <button className="button mt-20" type="submit" >Entrar</button>
+        </Link>
+
+      </div>
+
+    </div>
   )
 }
 
