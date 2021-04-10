382269bde0de703596dc20fb0650421dcaefc622
Daniel Gomes <danielgomes1@live.com>
2021-04-10 10:54:19 -0300

    wip: projeto front end Iniciado

--- front-gyra/src/App.js
+++ front-gyra/src/App.js
@@ -1,8 +1,43 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <h1>Hello World</h1>
+  const [messages, setMessages] = useState([
+    { id: 1, user: 'Daniel', message: 'Hello World' },
+    { id: 2, user: 'Daniel2', message: 'Hello World2' },
+    { id: 3, user: 'Daniel3', message: 'Hello World3' },
+
+  ]);
+
+  useEffect(() => {
+
+    const get = async () => {
+      const response = await fetch('http://localhost:3333/messages');
+      const data = await response.json();
+      setMessages(data);
+    };
+
+    get()
+
+  }, []);
+
+
+  const handleSendMessage = (user, message) => {
+    // sendMessage([...messages, { id: messages.length + 1, user: 'DanielNovo', message: 'New Message' }]);
+  }
+
+
+  return (
+    <>
+      <ul>
+        {messages.map(msg => <li key={msg.id}> {msg.user} <br></br> {msg.message} </li>)}
+      </ul>
+
+      <button onClick={() => handleSendMessage}>
+        Enviar Menssagem
+      </button>
+    </>
+
+  )
 }
 
 export default App;
